80f084af12f8ee9f93a6406206340bfbbaecd82f
Tilak J <tilak.j0321@outlook.com>
2023-10-11 21:16:34 +0530

    Edit and styles

--- singara/src/Components/Home.jsx
+++ singara/src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import '../Styles/Components/Home.scss'
 
 const Home = () => {
     const [values, setValues] = useState([]);
     const [newValue, setNewValue] = useState(""); 
-    const [editValue, setEditValue] = useState(""); 
-    const [editIndex, setEditIndex] = useState(-1); 
-    const baseUrl = "https://singaraapi.com/singaraapi"
+    const [editedValue, setEditedValue] = useState(""); 
+    const [editMode, setEditMode] = useState(null);
+    const baseUrl = "https://backend.singara.org";
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -28,7 +30,7 @@ const Home = () => {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body:JSON.stringify({ Value: newValue }),
+                body: JSON.stringify({ Value: newValue }),
             })
                 .then(() => {
                     fetchData();
@@ -40,31 +42,39 @@ const Home = () => {
         }
     };
 
-    const handleEdit = (index) => {
-        if (editValue && index >= 0) {
-            fetch(`${baseUrl}/api/home/${index}`, {
+    const handleEdit = (id, value) => {
+        // Check if the item is already in edit mode
+        if (editMode === id) {
+            // Save the edited value and exit edit mode
+            fetch(`${baseUrl}/api/home/${id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ Value: editValue }),
+                body: JSON.stringify({ Value: editedValue }),
             })
                 .then(() => {
                     fetchData();
-                    setEditValue("");
-                    setEditIndex(-1);
+                    setEditedValue("");
+                    setEditMode(null);
                 })
                 .catch((err) => {
                     console.log(err);
                 });
+        } else {
+            // Enter edit mode
+            setEditMode(id);
+            setEditedValue(value);
         }
     };
 
-    const handleDelete = (index) => {
-        console.log(index)
-        if (index >= 0) {
-            fetch(`${baseUrl}/api/home/${index}`, {
+    const handleDelete = (id) => {
+        if (id) {
+            fetch(`${baseUrl}/api/home/${id}`, {
                 method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
             })
                 .then(() => {
                     fetchData();
@@ -76,24 +86,33 @@ const Home = () => {
     };
 
     return (
-        <div>
+        <div className="container">
             <ul>
-                {console.log(values)}
                 <input
                     type="text"
                     placeholder="New Value"
                     value={newValue}
                     onChange={(e) => setNewValue(e.target.value)}
                 />
-                <button onClick={handleCreate}>Create</button>
-                {values.map((item,i)=>(
+                <button className={"save"} onClick={handleCreate}>Create</button>
+                {values.map((item, i) => (
                     <li key={i}>
-                        {item.Value}
-                        <button onClick={()=>handleDelete(item.Id)}>Delete</button>
-                        </li>
-                      
-                    ))}
-             </ul>
+                        {editMode === item.Id ? (
+                            <input
+                                type="text"
+                                value={editedValue}
+                                onChange={(e) => setEditedValue(e.target.value)}
+                            />
+                        ) : (
+                            item.Value
+                        )}
+                        <button  className={editMode === item.Id ? "save":"edit"} onClick={() => handleEdit(item.Id, item.Value)}>
+                            {editMode === item.Id ? "Save" : "Edit"}
+                        </button>
+                        <button className={"delete"}onClick={() => handleDelete(item.Id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
